b2e27e8cb00190eb900bb9ae030bff1b7dd066b4
Nam Chu Hoai <nambrot@googlemail.com>
2020-09-30 19:04:53 -0400

    Log the comment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
-export = (app: Application) => {
+export const app = (app: Application) => {
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
+  })
+
+  app.on('issue_comment', async (context) => {
+    console.log(context.payload.comment)
+
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
